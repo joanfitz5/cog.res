1d0d5b7bbbe0e15304b1f5af7eca13e76f135ecd
joanfitz5 <43268007+joanfitz5@users.noreply.github.com>
2020-11-28 15:44:22 +0000

    Update and rename Venn diagram to Venn_diagram.sh

--- Venn_diagram.sh
+++ Venn_diagram.sh
@@ -1,3 +1,3 @@
-http://bioinformatics.psb.ugent.be/webtools/Venn/
+# http://bioinformatics.psb.ugent.be/webtools/Venn/
 
 # extract gene symbols from gene mapping in FUMA for the three strategies and then from the GWGAS output. Input as directed into the venn diagram
